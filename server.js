24dd82ed288772e020eb94575e58ef03be2d81b6
John Gambrell <john.gambrell@gmail.com>
2024-05-31 11:12:49 -0500

    basic html stripping for product desc

--- server.js
+++ server.js
@@ -13,14 +13,29 @@ app.post('/format', formatBody, (req, res) => {
 function formatBody(req, res, next){
     var resArr = []
     var body = req.body
-    body.forEach((product) => {
-        const input = product.product_complete_description.toString() 
-        output = input.replace(/(<([^>]+)>)/ig, '')
+    body.forEach((p) => {
+        //const input = p.product_complete_description.toString() 
+        p.description = stripHTMLTags( p.description)
+        p.product_complete_description = stripHTMLTags( p.product_complete_description)
+        p.product_specifications = stripHTMLTags( p.product_specifications)
+        
         //output = input.replace(/(<([^>]+)>)/ig, '')
-        console.log(output)
-        resArr.push(output)
+        console.log(stripHTMLTags(p).toString())
+       // resArr.push(output)
     })
-    res.body = resArr
+    res.body = body
     next()
+}
+
+function stripHTMLTags(str) {
+    if ((str === null) || (str === ''))
+    return false;
+else
+    str = str.toString();
+
+// Regular expression to identify HTML tags in
+// the input string. Replacing the identified
+// HTML tag with a null string.
+return str.replace(/(<([^>]+)>)/ig, '');
 }
 app.listen(3000)
