ab3026594fc0f3e3f98a110c82696bd5b5f78868
John Gambrell <john.gambrell@gmail.com>
2024-05-31 11:12:51 -0500

    added addTOcart route and insert from req.body

--- server.js
+++ server.js
@@ -8,6 +8,25 @@ const express = require('express')
 
 
 const dynamo = DynamoDBDocument.from(new DynamoDB());
+
+require('dotenv').config()
+
+process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
+
+
+const knex = require('knex')({
+  client: 'pg',
+  connection: {
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+    port: 5432,
+    ssl: true
+  }
+});
+
+
 const app = express()
 
 app.use(express.json())
@@ -25,6 +44,27 @@ async function  getProductCatalog( req, res, next) {
     next()
 }
 
+//==================================================
+app.post('/addToCart', addToCart, (req, res)=> {
+    res.status(200).send(res.body)
+})
+
+
+async function  addToCart( req, res, next) {
+    console.log('addToCart called: ' + req.body)
+    knex('cart').insert({product: req.body.product, quantity: req.body.quantity, price: req.body.price})
+  .then(rows => {
+    res.body = {"rowCount": rows.rowCount, "command": rows.command}
+  })
+  .catch(err => {
+    console.error(err);
+  })
+  .finally(() => {
+    knex.destroy();
+    //console.log(res.body);
+    next()
+  });
+}
 
 
 app.listen(3000)
