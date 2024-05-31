9a5b06da1284327b7496c804b536dd53bed0c040
John Gambrell <john.gambrell@gmail.com>
2024-05-31 11:12:51 -0500

    add to cart js

--- route_handlers/add-to-cart.js
+++ route_handlers/add-to-cart.js
@@ -1,6 +1,4 @@
 const express = require('express')
-//process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
-
 
 exports.addToCart = knex =>  {
     
@@ -14,40 +12,7 @@ exports.addToCart = knex =>  {
       })
       .finally(() => {
         next()
-        //knex.destroy();
-        //console.log("knex has been destroyed.");
       });
 
   }
 }
-// exports.addToCart = async function  addToCart( req, res, next) { 
- 
-//     console.log('addToCart called: ' + req.body)
-
-//     // const knex = require('knex')({
-//     //   client: 'pg',
-//     //   connection: {
-//     //     host: process.env.HOST,
-//     //     user: process.env.DBUSER,
-//     //     password: process.env.PASSWORD,
-//     //     database: process.env.DATABASE,
-//     //     port: 5432,
-//     //     ssl: true
-//     //   },
-//     //   pool: { min: 2, max: 10 }
-//     // });
-
-//     knex('cart').insert({product: req.body.product, quantity: req.body.quantity, price: req.body.price})
-//   .then(rows => {
-//     res.body = {"rowCount": rows.rowCount, "command": rows.command}
-//   })
-//   .catch(err => {
-//     console.error(err);
-//   })
-//   .finally(() => {
-//     knex.destroy();
-//     //console.log(res.body);
-//     next()
-//   });
-
-// }
