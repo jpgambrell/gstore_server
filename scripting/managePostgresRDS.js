3e55049fcd263e571b3ad1767fd60913757d02d2
John Gambrell <john.gambrell@gmail.com>
2024-05-31 11:12:51 -0500

    connect to pg db and run query

---
+++ scripting/managePostgresRDS.js
@@ -0,0 +1,34 @@
+console.log('connecting to PostgresRDs')
+process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
+const {Client} = require('pg')
+
+// const client = () => {
+//   return new Client({
+//     "port": 5432,
+//     "user": "postgres",
+//     "password": "password1",
+//     "host": "gstore-database-1.c3qmtyytmzic.us-east-1.rds.amazonaws.com",
+//     "database": "cart"
+//   })
+// }
+
+//const uploadProductCatalogJSON = async () => {
+const executeQuery = async ()=> {
+  const client = await new Client({
+    "port": 5432,
+    "user": "postgres",
+    "password": "password1",
+    "host": "gstore-database-1.c3qmtyytmzic.us-east-1.rds.amazonaws.com",
+    "database": "cart",
+    "ssl": true
+  });
+  await client.connect();
+  let values = []
+  const result = await client.query("select * from cart");
+  console.log('anything in values? ' + values)
+  console.log("query res: " + JSON.stringify(result.rows))
+  await client.end();
+  return result;
+}
+executeQuery()
+
