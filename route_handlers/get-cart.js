bcb24656ca1642826ae9709b6922238cb7164a08
John Gambrell <john.gambrell@gmail.com>
2024-07-03 12:45:21 -0500

    get cart now ordered by update_dt desc

--- route_handlers/get-cart.js
+++ route_handlers/get-cart.js
@@ -1,7 +1,8 @@
 exports.getCart = knex => {
     return (req, res, next) => {
       console.log('getCart called')
-      knex.select().from('cart')
+      //knex.select().from('cart')
+      knex('cart').orderBy('update_dt', 'desc')
         .then(rows => {
           res.body = rows
         })
