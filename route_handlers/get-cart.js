bcbf4d39fd195d2601f0106ed4fe5e898c8e0be5
John Gambrell <john.gambrell@gmail.com>
2024-07-05 12:38:46 -0500

    new cartModel and fetching

--- route_handlers/get-cart.js
+++ route_handlers/get-cart.js
@@ -1,16 +1,46 @@
-exports.getCart = knex => {
+// exports.getCart = knex => {
+//     return (req, res, next) => {
+//       console.log('getCart called')
+//       //knex.select().from('cart')
+//       knex('cart').orderBy('update_dt', 'desc')
+//         .then(rows => {
+//           res.body = rows
+//         })
+//         .catch(err => {
+//           console.error(err);
+//         })
+//         .finally(() => {
+//           next()
+//         });
+//     }
+//   }
+
+  exports.getCart = knex => {
     return (req, res, next) => {
       console.log('getCart called')
-      //knex.select().from('cart')
-      knex('cart').orderBy('update_dt', 'desc')
+      knex('cart')
+      .join('cart_detail', 'cart.id', 'cart_detail.cart_id')
+      .orderBy('cart_detail.update_dt', 'desc')
         .then(rows => {
-          res.body = rows
+          const items = []
+          rows.forEach(element => {
+            console.log(element)
+            items.push({id: element.id, product_id: element.product_id, product_name: element.product_name, quantity: element.quantity, 
+              price: element.price, product_image_url: element.product_image_url, promotion: element.promotion});  
+          });
+          const result = {
+            id: rows[0].cart_id, 
+            promotion: rows[0].promotion,
+            user_id: rows[0].user_id,
+            items: items
+          };
+          res.body = result;
         })
         .catch(err => {
           console.error(err);
         })
         .finally(() => {
-          next()
+          next();
         });
-    }
+    };
   }
--- route_handlers/update-cart.js
+++ route_handlers/update-cart.js
@@ -12,10 +12,29 @@ exports.updateCart = knex =>  {
         res.statusCode = (rowsAffected == 0 ? 404 : 201)
   })
   .then(function()  {
-    knex.select().from('cart')
-    .then(rows => {
-      res.body = rows
-    })
+
+    // knex.select().from('cart')
+    // .then(rows => {
+    //   res.body = rows
+    // })
+    knex('cart')
+    .join('cart_detail', 'cart.id', 'cart_detail.cart_id')
+    .orderBy('cart_detail.update_dt', 'desc')
+      .then(rows => {
+        const items = []
+        rows.forEach(element => {
+          console.log(element)
+          items.push({id: element.id, product_id: element.product_id, product_name: element.product_name, quantity: element.quantity, 
+            price: element.price, product_image_url: element.product_image_url, promotion: element.promotion});  
+        });
+        const result = {
+          id: rows[0].cart_id, 
+          promotion: rows[0].promotion,
+          user_id: rows[0].user_id,
+          items: items
+        };
+        res.body = result;
+      })
     .catch(err => {
         console.error(err);
         res.statusCode = 501
