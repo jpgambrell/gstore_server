365bf474d7a84e1f5e67544184987212976cfd20
John Gambrell <john.gambrell@gmail.com>
2024-07-31 17:41:13 -0500

    removed modules

--- route_handlers/login.js
+++ route_handlers/login.js
@@ -1,20 +1,16 @@
 require('dotenv').config()
-//var Amplify = require('@aws-amplify/core');
 
-  
-  const AWS = require('aws-sdk')
+const { CognitoIdentityProvider } = require('@aws-sdk/client-cognito-identity-provider');
+const poolId = process.env.USER_POOL_ID
+const clientId = process.env.CLIENT_ID
 
-  AWS.config.update({region: 'us-east-1'});
-  
-
-  const poolId = process.env.USER_POOL_ID
-  const clientId = process.env.CLIENT_ID
-  
 exports.login = async (req, res, next) => { 
     console.log(`login called for ${req.body.user_name}`)
     const userName = req.body.user_name
     const password = req.body.password
-    const cognito = new AWS.CognitoIdentityServiceProvider();
+    const cognito = new CognitoIdentityProvider({
+        region: 'us-east-1'
+    });
 
 const params = {
     AuthFlow: 'USER_PASSWORD_AUTH',
@@ -26,7 +22,7 @@ const params = {
 };
 
     try {
-        const data = await cognito.initiateAuth(params).promise()
+        const data = await cognito.initiateAuth(params)
         res.body = data
 
         console.log(data)
@@ -37,26 +33,28 @@ const params = {
   next()
   }
 
-  exports.refresh = async (req, res, next) => {
-    const REFRESH_TOKEN = req.body.RefreshToken
-    const cognito = new AWS.CognitoIdentityServiceProvider();
+exports.refresh = async (req, res, next) => {
+  const REFRESH_TOKEN = req.body.RefreshToken
+  const cognito = new CognitoIdentityProvider({
+      region: 'us-east-1'
+  });
 
 const params = {
-    AuthFlow: 'REFRESH_TOKEN_AUTH',
-    ClientId: clientId,
-    AuthParameters: {
-        REFRESH_TOKEN
-    }
+  AuthFlow: 'REFRESH_TOKEN_AUTH',
+  ClientId: clientId,
+  AuthParameters: {
+      REFRESH_TOKEN
+  }
 }
 console.log('refresh called with tokens: ' + JSON.stringify(params))
 try {
-    const data = await cognito.initiateAuth(params).promise()
-    res.body = data
-    console.log(data)
+  const data = await cognito.initiateAuth(params)
+  res.body = data
+  console.log(data)
 }
 catch (err) {
-    console.log(err)
+  console.log(err)
 }
 next()
 
-  }
+}
--- route_handlers/signup.js
+++ route_handlers/signup.js
@@ -1,12 +1,5 @@
 require('dotenv').config()
-//var Amplify = require('@aws-amplify/core');
-
-  
-  const AWS = require('aws-sdk')
-
-
-
-  
+const { CognitoIdentityProvider } = require('@aws-sdk/client-cognito-identity-provider');
 exports.signup = async (req, res, next) => { 
     console.log(`login called for ${req.body.user_name}`)
 
@@ -16,8 +9,10 @@ exports.signup = async (req, res, next) => {
 
     const poolId = process.env.USER_POOL_ID
     const clientId = process.env.CLIENT_ID
-AWS.config.update({region: 'us-east-1'});
-const cognito = new AWS.CognitoIdentityServiceProvider();
+
+const cognito = new CognitoIdentityProvider({
+    region: 'us-east-1'
+});
 
 const params = {
     ClientId: clientId,
@@ -33,7 +28,7 @@ const params = {
 };
 
     try {
-        const data = await cognito.signUp(params).promise()
+        const data = await cognito.signUp(params)
         console.log(data)
         res.body = data
     }
